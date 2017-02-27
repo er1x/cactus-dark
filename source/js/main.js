ee5f95a055509448546ec8ed53f1c20ce4f59da0
Emilio J Grande <emilio@jsjutsu.com>
2017-02-27 19:25:57 +0100

    some customizations

--- source/js/main.js
+++ source/js/main.js
@@ -1,83 +1,109 @@
-if (!!$.prototype.justifiedGallery) {  // if justifiedGallery method is defined
-  var options = {
-    rowHeight: 140,
-    margins: 4,
-    lastRow: 'justify'
-  };
-  $('.article-gallery').justifiedGallery(options);
-}
+document.addEventListener('DOMContentLoaded', function () {
+  var $ = function (selector) {
+    return document.querySelector(selector);
+  }
 
-$(document).ready(function(){
-  $("#menu-icon, #menu-icon-tablet").click(function(){
-    if ( $('#menu').css('visibility') == 'hidden' ) {
-      $('#menu').css('visibility','visible');
-      $('#menu-icon, #menu-icon-tablet').addClass('active');
+  var hidden = function (elem) {
+    var width = elem.offsetWidth,
+        height = elem.offsetHeight;
 
-      var topDistance = $("#menu > #nav").offset().top;
+    return ( width === 0 && height === 0 ) ||
+           (((elem.style && elem.style.display) || getComputedStyle(elem)['display']) === "none");
+  }
 
-      if ( $('#menu').css('visibility') != 'hidden' && topDistance < 50 ) {
-        $("#menu > #nav").show();
-      } else if ($('#menu').css('visibility') != 'hidden' && topDistance > 100) {
-        $("#menu > #nav").hide();
-      }
-      return false;
+  var toggleVisibility = function (elem) {
+    if (elem.style.display == "block") {
+      elem.style.display = 'none';
     } else {
-      $('#menu').css('visibility','hidden');
-      $('#menu-icon, #menu-icon-tablet').removeClass('active');
-      return false;
+      elem.style.display = 'block';
     }
-  });
+  }
 
+  Array.from(document.querySelectorAll("#menu-icon, #menu-icon-tablet")).forEach(function (el) {
+    el.addEventListener('click', function () {
+      if ( getComputedStyle($('#menu'))['visibility'] === 'hidden' ) {
+        $('#menu').style.visibility = 'visible';
+        $('#menu-icon, #menu-icon-tablet').classList.add('active');
+
+        var topDistance = $("#menu > #nav").getBoundingClientRect().top + document.body.scrollTop;
+
+        if ( getComputedStyle($('#menu'))['visibility'] !== 'hidden' && topDistance < 50 ) {
+          $("#menu > #nav").style.display = '';
+        } else if (getComputedStyle($('#menu'))['visibility'] !== 'hidden' && topDistance > 100) {
+          $("#menu > #nav").style.display = 'none';
+        }
+        return false;
+      } else {
+        $('#menu').style.visibility = 'hidden';
+        $('#menu-icon, #menu-icon-tablet').classList.remove('active');
+        return false;
+      }
+    })
+  });
+  
+  if ($('#actions-footer #toc a')) {
+    $('#actions-footer #toc a').addEventListener('click', function (e) {
+      e.preventDefault();
+      toggleVisibility($('#toc-footer'))
+    });
+  }
+  if ($('#actions-footer #menu a')) {
+    $('#actions-footer #menu a').addEventListener('click', function (e) {
+      e.preventDefault();
+      toggleVisibility($('#nav-footer'))
+    });
+  }
+  
   /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-  $("#header > #nav > ul > .icon").click(function() {
-    $("#header > #nav > ul").toggleClass("responsive");
-  });
+  if ($("#header > #nav > ul > .icon")) {
+    $("#header > #nav > ul > .icon").addEventListener('click', function() {
+      $("#header > #nav > ul").classList.toggle("responsive");
+    });
+  }
 
-  if ( $( "#menu" ).length ) {
-    $(window).on('scroll', function() {
-      var topDistance = $("#menu > #nav").offset().top;
-
-      if ( $('#menu').css('visibility') != 'hidden' && topDistance < 50 ) {
-        $("#menu > #nav").show();
-      } else if ($('#menu').css('visibility') != 'hidden' && topDistance > 100) {
-        $("#menu > #nav").hide();
+  if ( $( "#menu" ) !== null ) {
+    window.addEventListener('scroll', function () {
+      var topDistance = $("#menu > #nav").getBoundingClientRect().top + document.body.scrollTop;
+      if ( getComputedStyle($('#menu'))['visibility'] !== 'hidden' && topDistance < 50 ) {
+        $("#menu > #nav").style.display = '';
+      } else if (getComputedStyle($('#menu'))['visibility'] !== 'hidden' && topDistance > 100) {
+        $("#menu > #nav").style.display = 'none';
       }
 
-      if ( ! $( "#menu-icon" ).is(":visible") && topDistance < 50 ) {
-        $("#menu-icon-tablet").show();
-        $("#top-icon-tablet").hide();
-      } else if (! $( "#menu-icon" ).is(":visible") && topDistance > 100) {
-        $("#menu-icon-tablet").hide();
-        $("#top-icon-tablet").show();
+      if ( hidden($('#menu-icon')) && topDistance < 50 ) {
+        $("#menu-icon-tablet").style.display = '';
+        $("#top-icon-tablet").style.display = 'none';
+      } else if (hidden($('#menu-icon')) && topDistance > 100) {
+        $("#menu-icon-tablet").style.display = 'none';
+        $("#top-icon-tablet").style.display = '';
       }
     });
   }
 
-  if ( $( "#footer-post" ).length ) {
+  if ( $( "#footer-post" ) !== null ) {
     var lastScrollTop = 0;
-    $(window).on('scroll', function() {
-      var topDistance = $(window).scrollTop();
+    window.addEventListener('scroll', function() {
+      var topDistance = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
 
-    if (topDistance > lastScrollTop){
-      // downscroll code
-      $("#footer-post").hide();
-    } else {
-      // upscroll code
-      $("#footer-post").show();
-    }
-    lastScrollTop = topDistance;
+      if (topDistance > lastScrollTop){
+        // downscroll code
+        $("#footer-post").style.display = 'none';
+      } else {
+        // upscroll code
+        $("#footer-post").style.display = '';
+      }
+      lastScrollTop = topDistance;
 
-    $("#nav-footer").hide();
-    $("#toc-footer").hide();
-    $("#share-footer").hide();
+      $("#nav-footer").style.display = 'none';
+      $("#toc-footer").style.display = 'none';
 
-    if ( topDistance < 50 ) {
-      $("#actions-footer > ul > #top").hide();
-      $("#actions-footer > ul > #menu").show();
-    } else if ( topDistance > 100 ) {
-      $("#actions-footer > ul > #menu").hide();
-      $("#actions-footer > ul > #top").show();
-    }
+      if ( topDistance < 50 ) {
+        $("#actions-footer > ul > #top").style.display = 'none';
+        $("#actions-footer > ul > #menu").style.display = '';
+      } else if ( topDistance > 100 ) {
+        $("#actions-footer > ul > #menu").style.display = 'none';
+        $("#actions-footer > ul > #top").style.display = '';
+      }
     });
   }
-});
+})
